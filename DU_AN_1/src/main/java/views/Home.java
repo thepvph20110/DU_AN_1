eb905ebd0bad45407082994341ee3c8c7e5a91e7
sydvph19885 <101080058+sydvph19885@users.noreply.github.com>
2022-12-04 15:33:57 +0700

    Update Home.java

--- DU_AN_1/src/main/java/views/Home.java
+++ DU_AN_1/src/main/java/views/Home.java
@@ -634,7 +634,7 @@ public class Home extends javax.swing.JFrame {
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(panelTong, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-
+        );
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
