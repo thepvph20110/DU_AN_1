c1c54a1f2eb58fbe3fad2f43f66eb250809839cf
caodinh <caococ2002@gmail.com>
2022-12-09 13:39:24 +0700

    gen ma

--- DU_AN_1/src/main/java/repository/IAcountRepository.java
+++ DU_AN_1/src/main/java/repository/IAcountRepository.java
@@ -24,7 +24,7 @@ public interface IAcountRepository {
 
     boolean delete(String id);
 
-    String genMaAccount();
+    int genMaAccount();
 
     Acount getByUseNameAndPass(String tenAccount, String matKhau);
 }
